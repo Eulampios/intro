9597e2e4d54d7ded34ce2914ef73e0dbd9477fe7
citramonus <riecco@mail.ru>
2023-10-15 19:23:01 +0300

    all exercise solved

---
+++ ex_1.cs
@@ -0,0 +1,45 @@
+﻿// /*
+// Внутри класса Answer напишите метод CompareNambers, который принимает на вход
+// два числа и выводит, какое число больше, какое меньше
+// */
+
+// using System;
+// public class Answer
+// {
+//     static void CompareNumbers(int firstNumber, int secondNumber)
+//     {
+//         // Введите свое решение ниже
+
+//         if (firstNumber < secondNumber)
+//         {
+//             Console.WriteLine($"Первое число {firstNumber} меньше чем второе число {secondNumber}");
+//         }
+//         else if (firstNumber > secondNumber)
+//         {
+//             Console.WriteLine($"Первое число {firstNumber} больше чем второе число {secondNumber}");
+//         }
+//         else
+//         {
+//             Console.WriteLine($"Введенные числа равны {secondNumber}");
+//         }
+
+//     }
+//     // Не удаляйте и не меняйте метод Main! 
+//     static public void Main(string[] args)
+//     {
+//         int firstNumber, secondNumber;
+//         if (args.Length >= 2)
+//         {
+//             firstNumber = int.Parse(args[0]);
+//             secondNumber = int.Parse(args[1]);
+//         }
+//         else
+//         {
+//             // Здесь вы можете поменять значения для отправки кода на Выполнение
+//             firstNumber = 5;
+//             secondNumber = 10;
+//         }
+//         // Не удаляйте строки ниже
+//         CompareNumbers(firstNumber, secondNumber);
+//     }
+// }
---
+++ ex_3.cs
@@ -0,0 +1,54 @@
+// /*
+// Внутри класса Answer напишите метод CheckIfEven, который на вход принимает число number и выводит, является ли число чётным (делится ли оно на два без остатка).
+
+// Пример использования:
+
+// На входе:
+// number: '6' 
+// На выходе:
+// Число `6` чётное 
+
+// На входе:
+// number: '3' 
+// На выходе:
+// Число `3` нечётное 
+
+// */
+
+// using System;
+// class Answer
+// {
+//     static void CheckIfEven(int number)
+//     {
+//         // Введите свое решение ниже
+//         if (number % 2 == 0)
+//         {
+//             System.Console.WriteLine($"Число `{number}` чётное");
+//         }
+//         else
+//         {
+//             System.Console.WriteLine($"Число `{number}` нечётное");
+//         }
+
+
+//     }
+
+//     // Не удаляйте и не меняйте метод Main! 
+//     static public void Main(string[] args)
+//     {
+//         int number;
+//         if (args.Length >= 1)
+//         {
+//             number = int.Parse(args[0]);
+//         }
+//         else
+//         {
+//             // Здесь вы можете поменять значения для отправки кода на Выполнение
+//             number = 5;
+//         }
+//         // Не удаляйте строки ниже
+//         CheckIfEven(number);
+//     }
+// }
+
+
