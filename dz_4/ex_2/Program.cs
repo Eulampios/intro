63dfeafc685c8ea73a25dce489c54a27a93fc844
citramonus <riecco@mail.ru>
2023-10-22 17:28:31 +0300

    ex_2 solved

---
+++ dz_4/ex_2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Напишите программу, которая принимает на вход 
+число и выдает сумму цифр в числе.
+*/
+
+
+int sumOfDigits(int number)
+{
+    int result = 0;
+
+    while (number > 0)
+    {
+        result += number % 10;
+        number /= 10;
+    }
+    return result;
+}
+
+
+System.Console.WriteLine(sumOfDigits(123));
+
