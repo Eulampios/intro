d4252f11db3d7d904910fc3388156c518ad74233
citramonus <riecco@mail.ru>
2023-10-22 17:13:32 +0300

    ex_3 solved

--- dz_3/ex_3/Program.cs
+++ dz_3/ex_3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Внутри класса Answer напишите метод ShowCube, который принимает
+на вход число (N) и выводит таблицу кубов чисел от 1 до N 
+(включительно) каждое на новой строке
+*/
+
+
+System.Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int result;
+
+for (int i = 1; i < N + 1; i++)
+{
+    result = i * i * i;
+    System.Console.WriteLine(result);
+}
