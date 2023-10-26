cdc167e137ffad891d0c4a77aba5fd5076c93eb1
citramonus <riecco@mail.ru>
2023-10-26 20:01:29 +0300

    ex_1 solved

--- dz_6/ex_1/Program.cs
+++ dz_6/ex_1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+int counter = 0;
+
+while (true)
+{
+    System.Console.WriteLine("введите число: ");
+    var textNumber = Console.ReadLine();
+    if (textNumber == "") break;
+    if (Convert.ToInt32(textNumber) > 0) counter++;
+}
+
+System.Console.WriteLine(counter);
+
